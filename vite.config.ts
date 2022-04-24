3e2f18fd3a8d5a4b3552eb0960b747ce3afea4b1
hedenica <hedenica@hotmail.com>
2022-04-24 11:18:46 -0300

    ✨ feat(setup): project initial setup

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import tsConfigPaths from 'vite-tsconfig-paths'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tsConfigPaths()
+  ]
+})
